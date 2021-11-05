43471247717631405cd3f6bc40dc0c9533616982
lucasmarcondes <lucas@marcondes.me>
2021-11-05 01:32:10 -0400

    setup login/register and specify padding in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Entries, Login, Signup, Templates, Analytics, Profile } from './containers'
+import { Entries, Login, Register, Templates, Analytics, Profile } from './containers'
 
 import { Header } from './components/default'
 import { Footer } from './components/default'
 
 import { useState } from 'react'
 
+import { Provider } from 'react-redux'
+import { store } from './store'
+
 export default function App() {
 	const [darkMode, setDarkMode] = useState(false)
 
 	return (
-		<BrowserRouter>
-			<div {...(darkMode && { className: 'dark' })}>
-				<Header onToggleDarkMode={() => setDarkMode(!darkMode)} darkMode={darkMode} />
-				<div className='flex flex-col min-h-[93vh]'>
-						<div className='flex-auto p-4 md:p-8 '>
+		<Provider store={store}>
+			<BrowserRouter>
+				<div className={`${darkMode ? 'dark ' : ''}flex flex-col h-screen`}>
+					<Header onToggleDarkMode={() => setDarkMode(!darkMode)} darkMode={darkMode} />
+					<div className='flex-1'>
 						<Switch>
 							<Route exact path={['/', '/entries']} component={Entries} />
 							<Route path='/login' component={Login} />
-							<Route path='/signup' component={Signup} />
+							<Route path='/register' component={Register} />
 							<Route path='/templates' component={Templates} />
 							<Route path='/analytics' component={Analytics} />
 							<Route path='/profile' component={Profile} />
 						</Switch>
 					</div>
+					<Footer />
 				</div>
-				<Footer />
-			</div>
-		</BrowserRouter>
+			</BrowserRouter>
+		</Provider>
 	)
 }
