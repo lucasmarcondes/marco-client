cbcb5c594ec7aee8fe54a259a9e7ad30fa60730e
eric marcondes <eric@marcondes.me>
2021-11-23 08:27:20 -0500

    notification menu

--- src/components/common/Notifications.tsx
+++ src/components/common/Notifications.tsx
@@ -1,14 +1,24 @@
 import { INotificationType } from '../../types'
-import { BsX, BsBell, BsCircle } from 'react-icons/bs'
-import { useRemoveNotificationMutation } from '../../store/api'
+import { BsX, BsBell, BsCircle, BsMailbox } from 'react-icons/bs'
+import { useGetUserQuery, useRemoveNotificationMutation } from '../../store/api'
 import { useState, useRef, useEffect } from 'react'
 
 export const Notifications = () => {
+	// const { data: user } = useGetUserQuery()
+	// const userColor = user?.preferences.accentColor ? user.preferences.accentColor : '#BFDBFF'
+	// const textColor = user?.preferences.textColor ? user.preferences.textColor : 'black'
 	const notificationsRef = useRef<HTMLInputElement>(null)
 	const [showNotifications, setShowNotifications] = useState(false)
 	const notifications: INotificationType[] = [
-		{ id: '1', message: 'test' },
-		{ id: '2', message: 'test' },
+		{
+			id: '6',
+			message: 'You need to confirm your email',
+		},
+		{ id: '1', message: 'This is a test', title: 'Test Notification' },
+		{ id: '2', message: 'This is a test', title: 'Test Notification' },
+		{ id: '3', message: 'This is a test', title: 'Test Notification' },
+		{ id: '4', message: 'This is a test', title: 'Test Notification' },
+		{ id: '5', message: 'This is a test', title: 'Test Notification' },
 	]
 
 	useEffect(() => {
@@ -23,19 +33,28 @@ export const Notifications = () => {
 
 	return (
 		<div ref={notificationsRef} className='flex'>
-			<button onClick={() => setShowNotifications(!showNotifications)} className='rounded-md my-auto mr-2 p-2 text-light-700 '>
-				<BsBell className='h-5 text-dark-200 w-5 block dark:(text-white ) ' />
+			<button onClick={() => setShowNotifications(!showNotifications)} className='rounded-md my-auto mr-5 p-2 text-light-700 '>
+				<span className='relative'>
+					<BsBell className='h-5 text-dark-200 w-5 block dark:(text-white ) ' />
+					{notifications.length > 0 && (
+						<span className='rounded-full font-bold bg-red-600 text-xs leading-none py-1 px-2 transform -top-5 -right-7 text-red-100 translate-x-1/2 -translate-y-1/2 absolute inline-flex items-center justify-center'>
+							{notifications.length}
+						</span>
+					)}
+				</span>
 			</button>
-			{notifications.length > 0 && <div className='rounded-full bg-red-600 h-2 top-3 right-16 w-2 absolute'></div>}
 			{showNotifications && (
 				<div className='divide-y bg-white rounded-md divide-gray-300 shadow-md mt-10 grid px-2 right-14 animate-fadeIn animate-animated w-3/9 z-50 grid-cols-1 absolute dark:( bg-gray-800 rounded-md text-light-300 ) '>
-					{notifications.map((notification, index) => {
-						return (
-							<div key={index}>
-								<Notification notification={notification} />
-							</div>
-						)
-					})}
+					<div className=' p-4 pb-2 '>Notifications</div>
+					<div className='divide-y divide-gray-300 h-80 overflow-y-auto'>
+						{notifications.map((notification, index) => {
+							return (
+								<div key={index}>
+									<Notification notification={notification} />
+								</div>
+							)
+						})}
+					</div>
 				</div>
 			)}
 		</div>
@@ -50,10 +69,11 @@ export const Notification = ({ notification }: Props) => {
 	const [removeNotifiction] = useRemoveNotificationMutation()
 
 	return (
-		<div className='bg-white flex w-full py-2 pb-4 overflow-hidden dark:(bg-gray-800) hover:(opacity-80)'>
-			<div className='-mx-3 py-2 px-4'>
-				<div className='mx-3'>
-					<p className='text-sm text-gray-600 dark:text-gray-200'>{notification.message}</p>
+		<a className='border-b flex -mx-2 py-3 px-4 items-center dark:(bg-gray-800 hover:bg-gray-700) hover:bg-gray-50 '>
+			<div className='flex -mx-3 py-2 px-4'>
+				<div className='mx-3 text-sm'>
+					<span className='font-bold mb-2'>{notification.title} </span>
+					<p className='text-gray-600 dark:text-gray-200'>{notification.message}</p>
 				</div>
 			</div>
 
@@ -64,6 +84,6 @@ export const Notification = ({ notification }: Props) => {
 					className='my-auto h-6 transition w-6 dark:(text-white) hover:(cursor-pointer text-gray-600 duration-150) '
 				/>
 			</div>
-		</div>
+		</a>
 	)
 }
