1760e01ddb6083f72a0b30c3857674f74ba7e02b
Lucas Marcondes <lucasepmarc@gmail.com>
2023-09-17 02:49:27 -0700

    make react 18 compatible

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import 'virtual:windi.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
-ReactDOM.render(
+createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+	  		<App />
 		</Provider>
 	</React.StrictMode>,
-	document.getElementById('root')
-)
+  )
